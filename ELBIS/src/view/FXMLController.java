7f6cf83d2e79b7d4dc6f1d5b74c0a77ccf2d350f
Dennis Schmidt <dennis2.schmidt@student.uni-siegen.de>
2020-12-08 15:21:50 +0100

    Added JavaFX Libraries

--- ELBIS/src/view/FXMLController.java
+++ ELBIS/src/view/FXMLController.java
@@ -1,5 +1,26 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class FXMLController {
 
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    void loginClicked(ActionEvent event) {
+
+    }
+
 }
+
