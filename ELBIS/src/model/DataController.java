5fbf72bde1252e11f38405107d1fb2217ab72f38
Uzay Avci <uzayavci@student.uni-siegen.de>
2020-12-09 21:21:50 +0100

    Barebone DataController Structure created.

--- ELBIS/src/model/DataController.java
+++ ELBIS/src/model/DataController.java
@@ -2,15 +2,16 @@ package model;
 import java.sql.*;
 
 public class DataController {
-	
+
 	// Test
-	
+
 	Connection con = null;
 	PreparedStatement pst = null;
-	
+
 	public void DBSendNewArticle() {
+		//newArticle:Article , should return boolean
 		con =  SQLConnection.ConnectDB();
-		try 
+		try
 		{
 			String query = "INSERT INTO Article (title, content, creationDate, expireDate, lastEdit, status, topic, authorId, publisherId, publisherComment)"
 					+ "VALUES ()";
@@ -18,14 +19,50 @@ public class DataController {
 			pst.execute();
 			con.close();
 		}
-		
+
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void DBSendNewTopic() {
-		
+
+	/*public boolean DBSendNewTopic() {
+		//newTopic:Topic
+
 	}
-	
+
+	public boolean DBSendNewUser(){
+		//newUser:User
+
+	}
+
+	public Article DBLoadArticle(){
+		//dbrequest:String
+
+	}
+	public Topic DBLoadTopic(){
+		//dbrequest:String
+
+	}
+	public User DBLoadUser(){
+		//dbrequest:String
+
+	}
+	public boolean DBEditArticle(){
+		//dbrequest:String
+
+	}
+	public boolean DBEditUser(){
+		//dbrequest:String
+
+	}
+	public boolean DBEditTopic(){
+		//dbrequest:String
+
+	}
+	public LinkedList<Article> DBLoadRecentArticle(){
+		//dbrequest:String
+
+	}
+	*/
+
 }
