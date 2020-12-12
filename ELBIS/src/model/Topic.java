eaae76a70a91182e277bea49b8ab58e3103e20e4
Uzay Avci <uzayavci@student.uni-siegen.de>
2020-12-12 18:40:52 +0100

    Topic class somewhat initialized.

--- ELBIS/src/model/Topic.java
+++ ELBIS/src/model/Topic.java
@@ -1,4 +1,31 @@
 package model;
 
 public class Topic {
+    private int id;
+    private String name;
+    private int parentTopic;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getParentTopic() {
+        return parentTopic;
+    }
+
+    public void setParentTopic(int parentTopic) {
+        this.parentTopic = parentTopic;
+    }
 }
