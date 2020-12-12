d864ba2c6eca099279ae96fa3d8888a56b40a501
Uzay Avci <uzayavci@student.uni-siegen.de>
2020-12-12 18:37:02 +0100

    Test class for DataController initialized.

--- ELBIS/src/model/Test.java
+++ ELBIS/src/model/Test.java
@@ -1,8 +1,31 @@
 package model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
 
+
+        DataController dataController = new DataController();
+        if(!dataController.open()) {
+            System.out.println("Can't open datacontroller");
+            return;
+        }
+
+
+
+        dataController.DBSendNewArticle("Hello World","This is Mambo no.5","Hell Yeah");
+
+        dataController.close();
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter an Article title: ");
+//        String title = scanner.nextLine();
+
+
+
     }
 }
+
