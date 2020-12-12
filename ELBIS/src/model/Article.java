8b7c5cc7b6b8a5c56cb8fea1db3c02676d544411
Uzay Avci <uzayavci@student.uni-siegen.de>
2020-12-12 18:35:35 +0100

    Article Class somewhat initialized.

--- ELBIS/src/model/Article.java
+++ ELBIS/src/model/Article.java
@@ -1,6 +1,70 @@
 package model;
 
+import java.util.Date;
+
 public class Article {
 
+    private int id;
+    private String title;
+    private String topic;
+    private String content;
+    private String expireDate;
+    private String creationDate;
+    private String publisherComment;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(String expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getPublisherComment() {
+        return publisherComment;
+    }
+
+    public void setPublisherComment(String publisherComment) {
+        this.publisherComment = publisherComment;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
 }
