ab3cbf671a314ca0cfeb35e85c36d84f8822b478
Uzay Avci <uzayavci@student.uni-siegen.de>
2020-12-12 19:31:22 +0100

    Test succesful.

--- ELBIS/src/model/Test.java
+++ ELBIS/src/model/Test.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,8 +17,10 @@ public class Test {
         }
 
 
-
-        dataController.DBSendNewArticle("Hello World","This is Mambo no.5","Hell Yeah");
+        User user = dataController.DBLoadUser(0);
+        System.out.println(user.getName());
+        System.out.println(user.geteMail());
+        dataController.DBLoadUser(5);
 
         dataController.close();
 
@@ -25,6 +29,36 @@ public class Test {
 //        String title = scanner.nextLine();
 
 
+        /*
+        public User DBLoadUser(int id) {
+            try {
+                LoadUser.setInt(1, id);
+                LoadUser.execute();
+                ResultSet rs = LoadUser.getResultSet();
+                String name = null;
+                String email= null;
+                while (rs.next()) {
+                    //user.setId(id);
+                    //user.seteMail(rs.getString(2));
+                    //user.setName(rs.getString(5));
+
+                    //System.out.println(rs.getString(2));
+                    name = rs.getString(2);
+                    email = rs.getString(1);
+
+                    //user.setGender(rs.getInt(7));
+                    //user.setDateOfBirth(rs.getDate(8));
+                }
+                User user = new User(id,name,email,null,null,null);
+                return user;
+            } catch (SQLException e) {
+                System.out.println("Couldn't load User: " + e.getMessage());
+                return null;
+            }
+
+
+        }
+        */
 
     }
 }
