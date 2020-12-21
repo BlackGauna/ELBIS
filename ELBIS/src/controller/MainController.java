70d138a55d9f42ee7db7a6e2b9b905c5c40f104a
Dennis Schmidt <dennis2.schmidt@student.uni-siegen.de>
2020-12-21 14:54:12 +0100

    Deleted View_Test; application now launches from MainController

--- ELBIS/src/controller/MainController.java
+++ ELBIS/src/controller/MainController.java
@@ -1,8 +1,11 @@
 package controller;
 
+import javafx.application.Application;
 import model.DataController;
 import model.User;
+import view.MainView;
 
+import javax.swing.*;
 import java.sql.SQLException;
 
 public class MainController{
@@ -15,16 +18,24 @@ public class MainController{
 	public static void main(String[] args) {
 		new MainController();
 
+		//Start the FX application
+		try {
+			Application.launch(MainView.class);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null,e.getMessage(),"Error on launch in View_TEST", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
 	}
 	
 	//ctor_______________________________________________________________________________________________________
 	public MainController() {
-		
+
 	}
 	
 	//Methods_______________________________________________________________________________________________________
 	public boolean login(String email, String pw) throws SQLException {
 		return dc.login(email, pw);
+		//TODO set active User
 	}
 
 	//Getters,Setters_______________________________________________________________________________________________________
