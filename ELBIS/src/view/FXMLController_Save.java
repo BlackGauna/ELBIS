d23afd81d099084aed1971b2863addcac2ea3066
Onur Hokkaömeroglu <onur.hokkaoemeroglu@student.uni-siegen.de>
2020-12-30 12:41:42 +0100

    Updated editor save method. Now loading expireDate of article if set before. Deleted old editor classes as not used anymore.

--- ELBIS/src/view/FXMLController_Save.java
+++ ELBIS/src/view/FXMLController_Save.java
@@ -14,6 +14,7 @@ import model.Status;
 import model.Topic;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
@@ -65,6 +66,12 @@ public class FXMLController_Save
     {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         return expireDate.getValue().format(formatter);
+    }
+
+    public void setExpireDate(String dateString)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        expireDate.setValue(LocalDate.parse(dateString, formatter));
     }
 
 }
