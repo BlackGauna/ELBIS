197c07d052094956e700c880fdc5a172fd4ac3ee
Uzay Avci <uzayavci@student.uni-siegen.de>
2021-01-03 19:00:22 +0100

    TODOs updated

--- ELBIS/src/controller/TODOs.java
+++ ELBIS/src/controller/TODOs.java
@@ -16,12 +16,9 @@ public class TODOs {
 
     //_____DB_____
     /*
-    TODO Methods to permit and de-permit Topics to users
-    TODO Method to fetch all topics by a specific user
     TODO Update loadUser methods to update the topic List of a user
-    TODO Methods to Delete Users/Topic/Articles fully from Database
-    TODO Method updateDatabase to be called in dataController constructor (gets every article and sets the status properly - compares teh dates ... for example if expired)
-
+    TODO Method updateAllArticles to be called in dataController constructor (gets every article and sets the status properly - compares teh dates ... for example if expired)
+    TODO update Article and Topic classes to hold a topic object instead of just an id of a topic  (old int type topic attributes still there for compatibility)
     TODO maybe a method to just change the status of an article (with no chance to change anything else)
      */
 
