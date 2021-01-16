489d3bc190aca39e38e5115cba440f7c28386d0a
Dominik Tautz <dominik.tautz@student.uni-siegen.de>
2021-01-16 09:08:44 +0100

    added routers to server.js

--- elbisweb/backend/server.js
+++ elbisweb/backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = "mongodb+srv://admin:admin@elbis8.ilafq.mongodb.net/ELBIS?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
 );
 mongoose.connection.once('open', () => {
     console.log("MongoDB connected");
@@ -20,12 +20,17 @@ mongoose.connection.once('open', () => {
 const userRouter = require('./routes/user');
 const articleRouter = require('./routes/article');
 const topicRouter = require('./routes/topic');
+const roleRouter = require('./routes/role');
+const genderRouter = require('./routes/gender');
+const statusRouter = require('./routes/status');
 
 
 app.use('/user', userRouter);
 app.use('/article', articleRouter);
 app.use('/topic', topicRouter);
-
+app.use('/role', roleRouter);
+app.use('gender', genderRouter);
+app.use('status', statusRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
