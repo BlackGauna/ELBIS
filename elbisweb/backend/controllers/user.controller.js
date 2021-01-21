1d52a5a762cdae09903623f57a4af53fa9076bbe
Dominik Tautz <dominik.tautz@student.uni-siegen.de>
2021-01-21 19:26:08 +0100

    Umstrukturierung Routes/Datenbankabfragen. Mache das gleiche morgen noch für Artikel usw. Ist so übersichtlicher als vorher.

---
+++ elbisweb/backend/controllers/user.controller.js
@@ -0,0 +1,113 @@
+let User = require('../models/user.model');
+
+// Create and save an ew User
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.email) {
+        res.status(400).send({message: "Content can not be empty!"});
+        return;
+    }
+
+    // Create a user
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role,
+        name: req.body.name,
+        address: req.body.address,
+        gender: req.body.gender,
+        dateOfBirth: req.body.dateOfBirth
+    });
+
+    // Save user in database
+    user
+        .save(user)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occured while creating the user."
+            });
+        });
+};
+
+// Retrieve all Users from the database
+exports.findAll = (req, res) => {
+    User.find()
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                err.message || "Some error occured while retrieving users."
+            });
+        });
+};
+
+// Find a single User with an ID
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    User.findById(id)
+        .then(data => {
+            if (!data)
+                res.status(404).send({message: "Not found user with id " + id});
+            else res.send(data);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send({message: "Error retrieving user with id " + id});
+        });
+};
+
+// Update a user by the id
+exports.update = (req, res) => {
+    if (!req.body){
+        return res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
+
+    const id = req.params.id;
+
+    User.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+        .then(data => {
+            if (!data){
+                res.status(404).send({
+                    message: "Cannot update User with id = " + id + ". Maybe User was not found!"
+                });
+            } else res.send({message: "User was updated successfully."});
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating User with id " + id
+            });
+        });
+};
+
+// Delete User with ID
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    User.findByIdAndRemove(id, {useFindAndModify: false})
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: "Cannot delete User with id " + id + ". Maybe User was not found"
+                });
+            } else {
+                res.send({
+                    message: "User was deletet successfully!"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete User with id " + id
+            });
+        });
+}
--- elbisweb/backend/server.js
+++ elbisweb/backend/server.js
@@ -17,20 +17,19 @@ mongoose.connection.once('open', () => {
     console.log("MongoDB connected");
 })
 
-const userRouter = require('./routes/user');
+const userRouter = require('./routes/user.route');
 const articleRouter = require('./routes/article');
 const topicRouter = require('./routes/topic');
 const roleRouter = require('./routes/role');
 const genderRouter = require('./routes/gender');
 const statusRouter = require('./routes/status');
 
-
 app.use('/user', userRouter);
 app.use('/article', articleRouter);
 app.use('/topic', topicRouter);
 app.use('/role', roleRouter);
-app.use('gender', genderRouter);
-app.use('status', statusRouter);
+app.use('/gender', genderRouter);
+app.use('/status', statusRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
