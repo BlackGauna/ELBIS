7784ee511f7065415179f26eeb8e4cb01a1c71fd
Dominik Tautz <dominik.tautz@student.uni-siegen.de>
2021-01-24 10:15:03 +0100

    createUser design update

--- elbisweb/frontend/src/components/moderation/moderation_createUser.component.js
+++ elbisweb/frontend/src/components/moderation/moderation_createUser.component.js
@@ -22,7 +22,10 @@ export default class moderation_createUser extends Component {
         this.onChange_passwordCheck = this.onChange_passwordCheck.bind(this);
         this.onChange_foreName = this.onChange_foreName.bind(this);
         this.onChange_surName = this.onChange_surName.bind(this);
-        this.onChange_address = this.onChange_address.bind(this);
+        this.onChange_street = this.onChange_street.bind(this);
+        this.onChange_houseNumber = this.onChange_houseNumber.bind(this);
+        this.onChange_plz = this.onChange_plz.bind(this);
+        this.onChange_place = this.onChange_place.bind(this);
         this.onChange_gender = this.onChange_gender.bind(this);
         this.onChange_role = this.onChange_role.bind(this);
         this.onChange_bday = this.onChange_bday.bind(this);
@@ -35,7 +38,10 @@ export default class moderation_createUser extends Component {
             passwordCheck: '',
             foreName: '',
             surName: '',
-            address: '',
+            street: '',
+            houseNumber: '',
+            plz: '',
+            place: '',
             gender: [],
             role: [],
             // bDay: new Date(),
@@ -81,50 +87,112 @@ export default class moderation_createUser extends Component {
             <div className="container">
                 <h3>Nutzer erstellen</h3>
                 <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>E-Mail: </label>
-                        <br/>
-                        <input type="email" className="form-control" value={this.state.email}
-                               onChange={this.onChange_email}/>
-                    </div>
-
-                    <div className="form-group">
-                        <label>passwort: </label>
-                        <br/>
-                        <input type="password" className="form-control" value={this.state.password}
-                               onChange={this.onChange_password}/>
-                    </div>
-                    <div className="form-group">
-                        <label>passwort bestätigen: </label>
-                        <br/>
-                        <input type="password" className="form-control" value={this.state.passwordCheck}
-                               onChange={this.onChange_passwordCheck}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Vorname: </label>
-                        <br/>
-                        <input type="name" className="form-control" value={this.state.foreName}
-                               onChange={this.onChange_foreName}/>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label>Vorname</label>
+                            <input
+                                type="name"
+                                className="form-control"
+                                placeholder="Vorname"
+                                value={this.state.foreName}
+                                onChange={this.onChange_foreName}/>
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label>Nachname</label>
+                            <input
+                                type="name"
+                                className="form-control"
+                                placeholder="Nachname"
+                                value={this.state.surName}
+                                onChange={this.onChange_surName}/>
+                        </div>
                     </div>
                     <div className="form-group">
-                        <label>Nachname: </label>
-                        <br/>
-                        <input type="name" className="form-control" value={this.state.surName}
-                               onChange={this.onChange_surName}/>
+                        <label>E-Mail</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            placeholder="E-Mail"
+                            value={this.state.email}
+                            onChange={this.onChange_email}/>
                     </div>
-                    <div className="form-group">
-                        <label>Anschrift: </label>
-                        <br/>
-                        <input type="address" className="form-control" value={this.state.address}
-                               onChange={this.onChange_address}/>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label>Passwort</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Passwort"
+                                value={this.state.password}
+                                onChange={this.onChange_password}/>
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label>Passwort bestätigen</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Passwort"
+                                value={this.state.passwordCheck}
+                                onChange={this.onChange_passwordCheck}/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label>Geschlecht: </label>
-                        <Select options={this.state.gender} onChange={this.onChange_gender}/>
+                    <div className="form-row">
+                        <div className="form-group col-md-10">
+                            <label>Straße</label>
+                            <input
+                                type="street"
+                                className="form-control"
+                                placeholder="Straße"
+                                value={this.state.street}
+                                onChange={this.onChange_street}/>
+                        </div>
+                        <div className="form-group col-md-2">
+                            <label>Hausnummer</label>
+                            <input
+                                type="houseNumber"
+                                className="form-control"
+                                placeholder="Hausnummer"
+                                value={this.state.houseNumber}
+                                onChange={this.onChange_houseNumber}/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label>Rolle: </label>
-                        <Select options={this.state.role} onChange={this.onChange_role}/>
+                    <div className="form-row">
+                        <div className="form-group col-md-3">
+                            <label>PLZ</label>
+                            <input
+                                type="plz"
+                                className="form-control"
+                                placeholder="PLZ"
+                                value={this.state.plz}
+                                onChange={this.onChange_plz}/>
+                        </div>
+                        <div className="form-group col-md-9">
+                            <label>Ort</label>
+                            <input
+                                type="ort"
+                                className="form-control"
+                                placeholder="Ort"
+                                value={this.state.place}
+                                onChange={this.onChange_place}/>
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label>Geschlecht</label>
+                            <Select
+                                type="geschlecht"
+                                placeholder="Geschlecht"
+                                options={this.state.gender}
+                                onChange={this.onChange_gender}/>
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label>Rolle</label>
+                            <Select
+                                type="role"
+                                placeholder="Rolle"
+                                options={this.state.role}
+                                onChange={this.onChange_role}/>
+                        </div>
                     </div>
                     <div className="form-group">
                         <input type="submit" value="Nutzer erstellen" className="btn btn-primary"/>
@@ -144,7 +212,7 @@ export default class moderation_createUser extends Component {
             //TODO make password check
             password: this.state.password,
             name: this.state.foreName + " " + this.state.surName,
-            address: this.state.address,
+            address: this.state.street + " " + this.state.houseNumber + ", " + this.state.plz + " " + this.state.place,
             gender: this.state.gender,
             role: this.state.role,
             bDay: this.state.bday
@@ -156,7 +224,7 @@ export default class moderation_createUser extends Component {
                     email: res.data.email,
                     password: res.data.password,
                     name: res.data.foreName + " " + res.data.surName,
-                    address: res.data.address,
+                    address: res.data.street + " " + res.data.houseNumber + ", " + res.data.plz + " " + res.data.place,
                     gender: res.data.gender,
                     role: res.data.role,
                     // TODO: bday
@@ -204,9 +272,27 @@ export default class moderation_createUser extends Component {
         })
     }
 
-    onChange_address(e) {
+    onChange_street(e) {
         this.setState({
-            address: e.target.value
+            street: e.target.value
+        })
+    }
+
+    onChange_houseNumber(e) {
+        this.setState({
+            houseNumber: e.target.value
+        })
+    }
+
+    onChange_plz(e) {
+        this.setState({
+            plz: e.target.value
+        })
+    }
+
+    onChange_place(e) {
+        this.setState({
+            place: e.target.value
         })
     }
 
