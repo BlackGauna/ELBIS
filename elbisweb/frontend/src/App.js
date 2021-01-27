8d23beb48a5ed56e64f4692f0e18e5e8a8d1adff
Dominik Tautz <dominik.tautz@student.uni-siegen.de>
2021-01-27 15:25:25 +0100

    Revert "Routes are now managed by the navbar"
    This reverts commit 9adf8539

--- elbisweb/frontend/src/App.js
+++ elbisweb/frontend/src/App.js
@@ -6,9 +6,20 @@ import loggedUser from './session/loggedUser';
 import './ELBISWeb.css';
 import logo from './resources/ELBIS_logo/ELBIS_Ausgeschrieben.png';
 import NavBar from "./components/ELBIS_navbar.component";
+import ELBISweb from "./components/index.component";
+import manageAccount from "./components/manageAccount.component";
+import userView from "./components/user/user_myArticles.Component";
+import createUser from "./components/moderation/moderation_createUser.component";
 import loginView from "./components/loginView.component";
+import CreateArticle from "./components/user/CreateArticle.component";
+import createTopic from "./components/administration/administration_createTopic.component";
+import manageRoles from "./components/administration/administration_userList.component";
+import allArticlesList from "./components/moderation/moderation_articleList.component";
+import manageSubmissions from "./components/moderation/moderation_submissionList.component";
+import editUser from "./components/moderation/moderation_editUser.component";
 import SessionDataService from "./services/session.service";
-
+import moderation_userList from "./components/moderation/moderation_userList.component";
+import administration_topicList from "./components/administration/administration_topicList.component";
 
 //##########App start##########
 /*
@@ -32,12 +43,70 @@ class App extends React.Component {
                 </div>
             )
         } else {
+            //TODO resolve redundancies
             if (loggedUser.isLoggedIn) {
-                return (<div className="app">
-                    <Router>
-                        <Route path="/" component={NavBar}/>
-                    </Router>
-                </div>);
+                if (sessionStorage.getItem("sessionRole") === "Administrator") {
+                    /*************
+                     *   Admin Area
+                     * *************/
+                    return (
+                        <div className="app">
+                            <Router>
+                                <Route path="/" component={NavBar}/>
+                                <br/>
+                                <Route exact path="/login/home" component={ELBISweb}/>
+                                <Route exact path="/login/manageAccount" component={manageAccount}/>
+                                <Route exact path="/login/edit" component={CreateArticle}/>
+                                <Route exact path="/login/edit/:id" component={CreateArticle}/>
+                                <Route exact path="/login/user/myArticles" component={userView}/>
+                                <Route exact path="/login/mod/manageSubmissions" component={manageSubmissions}/>
+                                <Route exact path="/login/mod/manageArticles" component={allArticlesList}/>
+                                <Route exact path="/login/mod/manageUsers" component={moderation_userList}/>
+                                <Route exact path="/login/mod/createUser" component={createUser}/>
+                                <Route exact path="/login/mod/editUser/:id" component={editUser}/>
+                                <Route exact path="/login/admin/manageTopics" component={administration_topicList}/>
+                                <Route exact path="/login/admin/createTopic" component={createTopic}/>
+                                <Route exact path="/login/admin/manageRoles" component={manageRoles}/>
+                            </Router>
+                        </div>
+                    )
+                } else if (sessionStorage.getItem("sessionRole") === "Moderator") {
+                    /*************
+                     *   Mod Area
+                     * *************/
+                    return (<div className="app">
+                        <Router>
+                            <Route path="/" component={NavBar}/>
+                            <br/>
+                            <Route exact path="/login/home" component={ELBISweb}/>
+                            <Route exact path="/login/manageAccount" component={manageAccount}/>
+                            <Route exact path="/login/edit" component={CreateArticle}/>
+                            <Route exact path="/login/edit/:id" component={CreateArticle}/>
+                            <Route exact path="/login/user/myArticles" component={userView}/>
+                            <Route exact path="/login/mod/manageSubmissions" component={manageSubmissions}/>
+                            <Route exact path="/login/mod/manageArticles" component={allArticlesList}/>
+                            <Route exact path="/login/mod/manageUsers" component={moderation_userList}/>
+                            <Route exact path="/login/mod/createUser" component={createUser}/>
+                            <Route exact path="/login/mod/editUser/:id" component={editUser}/>
+                        </Router>
+                    </div>);
+                } else if (sessionStorage.getItem("sessionRole") === "User") {
+                    /*************
+                     *   User Area
+                     * *************/
+                    return (<div className="app">
+                        <Router>
+                            <Route path="/" component={NavBar}/>
+                            <br/>
+                            <Route exact path="/login/home" component={ELBISweb}/>
+                            <Route exact path="/login/manageAccount" component={manageAccount}/>
+                            <Route exact path="/login/edit" component={CreateArticle}/>
+                            <Route exact path="/login/edit/:id" component={CreateArticle}/>
+                            <Route exact path="/login/user/myArticles" component={userView}/>
+                        </Router>
+                    </div>);
+                } else {
+                }
             } else if (!loggedUser.isLoggedIn) {
                 return (
                     <div className="app">
