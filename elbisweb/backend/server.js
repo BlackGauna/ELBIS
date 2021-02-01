81810c8e1614d8d4ecf78438aceea5446a4f0c2f
Uzay Avci <uzayavci@student.uni-siegen.de>
2021-02-01 13:06:47 +0100

    Small bugfix

--- elbisweb/backend/server.js
+++ elbisweb/backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const now = new Date();
+const now = new Date().toISOString();
 
 app.use(cors());
 app.use(express.json());
