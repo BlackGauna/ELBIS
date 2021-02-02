c921bd44b495321ec003366e2671464777e2d2d0
Dominik Tautz <dominik.tautz@student.uni-siegen.de>
2021-02-02 18:28:52 +0100

    deleted TODOs in models.

--- elbisweb/backend/models/article.model.js
+++ elbisweb/backend/models/article.model.js
@@ -3,21 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
-    //TODO Limits to variables (Trim,minlength etc)
-    title: {type: String, required:  true},
-    path: {type: String, required:  true},
-    //TODO should expire date be required ?
-    expireDate: {type: Date, required:  false},
+    title: {type: String, required: true, unique: true},
+    path: {type: String, required: true},
+    expireDate: {type: Date, required: true},
     status: {type: String, required: true},
-    //TODO make sure the topicSchema import works
-    // topic: {type: mongoose.Schema.Types.ObjectId, ref: 'Topic', required:  true},
     topic: {type: String, required: true},
     author: {type: String, required: true},
     publisher: {type: String, required: false},
-    // authorId: {type: Number, required:  true},
-    // publisherId: {type: Number, required:  false},
-    publisherComment: {type: String, required:  false},
-
+    publisherComment: {type: String, required: false},
 }, {
     timestamps: true,
 });
