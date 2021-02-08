85847c16847726198c2bbd996cba1eaf86cc6d07
Dennis Schmidt <dennis2.schmidt@student.uni-siegen.de>
2021-02-08 13:01:02 +0100

    todos updated

--- elbisweb/frontend/src/TODO.js
+++ elbisweb/frontend/src/TODO.js
@@ -5,8 +5,6 @@
 TODO MAYBE deploy the project to a free hosting server at the end ?
 TODO MAYBE learn how to export it as an app to phones because its really cool
 
-TODO Moderators and Admins can use All topics (but they can still hav entries in userTopics - for the case they get degraded to a user)
-
 -------DB/Backend-------
 
 -------Main/Frontend-------
@@ -15,11 +13,11 @@ TODO Handle Text overflow on cells (cellformatter?)
 TODO MAYBE Make a public/submitted article revertable (unpublish) and the comment editable
 
 SidePages
-TODO finalize 'editUser'
+TODO finalize 'editUser' and load initial userTopics correctly
 TODO password reset for logged and nonn logged users
 TODO make sure all modals close on submit
+TODO dont show routine based status on article creation for admins/mods
 
 -------Editor-------
-TODO save article if one of the meta-fields is changed (not just on content update)
 
  */
