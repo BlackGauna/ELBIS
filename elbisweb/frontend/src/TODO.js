a89b28266fda39aefb972cac47843196d1e3a4dd
Dennis Schmidt <dennis2.schmidt@student.uni-siegen.de>
2021-02-10 03:06:43 +0100

    Testing and TODO update

--- elbisweb/frontend/src/TODO.js
+++ elbisweb/frontend/src/TODO.js
@@ -13,11 +13,16 @@ TODO WIP fix routes to not show navbar in terminal and article previews
 Tables
 TODO Handle Text overflow on cells (cellformatter?)
 TODO MAYBE Make a public/submitted article revertable (unpublish) and the comment editable
+TODO show publish date and maybe remove creation date from article Tables
+TODO add a preview to all article tables
 
 SidePages
 TODO finalize 'editUser' and load initial userTopics correctly
 TODO password reset for logged and nonn logged users
 
 -------Editor-------
+TODO QR-Code formatting is bugged
+TODO Old publish date gets not loaded properly on opening of the editor
+TODO pictures are quickly oversized and are not manually resizeable at all (even if I do so in the Editor)
 
  */
